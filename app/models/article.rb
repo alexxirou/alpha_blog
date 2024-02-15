f867dc1cde5dbc69f58a432cbf5f34f7f003df29
Alex <alex.xirou@hotmail.com>
2024-02-15 19:42:19 +0200

    fixed bugs due to misspells and article attri  being nil when creating a form with no preexisting articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :descripton, presence: true, length: { minimum: 10, maximum: 2500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 2500 }
 end
