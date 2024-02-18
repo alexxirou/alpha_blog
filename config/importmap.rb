9bf8c517679c975bb1cb654a0d0f7bc0a2ad076d
Alex <alex.xirou@hotmail.com>
2024-02-18 16:11:36 +0200

    added bootstrap support and styles for the home page and article edit/new forms

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,10 @@
-# Pin npm packages by running ./bin/importmap
+# config/importmap.rb
 
-pin "application"
+pin_all_from "app/javascript", under: "javascript"
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js",  preload: true
+pin_all_from "app/javascript/controllers", to: "controllers"
+pin "@popperjs/core", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
